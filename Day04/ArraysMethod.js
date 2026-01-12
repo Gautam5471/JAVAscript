d8c3d80093ead38e82aa1cfe0a5e1b04464b9ce2
Gautam kumar <gautamkumar2020x@gmail.com>
2026-01-13 00:22:20 +0530

    Arrays

--- Day04/ArraysMethod.js
+++ Day04/ArraysMethod.js
@@ -14,13 +14,50 @@
 
 // 
 
-let marvel_heroes = ["thor","spiderman","ironman"];
-let dc_heroes = ["superman","batman"];
-let indian_heros=["shaktiamn","krish"]
+// let marvel_heroes = ["thor","spiderman","ironman"];
+// let dc_heroes = ["superman","batman"];
+// let indian_heros=["shaktiamn","krish"]
 
-let heroes=marvel_heroes.concat(dc_heroes).concat(indian_heros);
-let heroes2=marvel_heroes.concat(dc_heroes,indian_heros)
-console.log(heroes)
-console.log(heroes2)
-console.log(dc_heroes)
-console.log(marvel_heroes)
+// let heroes=marvel_heroes.concat(dc_heroes).concat(indian_heros);
+// let heroes2=marvel_heroes.concat(dc_heroes,indian_heros)
+// console.log(heroes)
+// console.log(heroes2)
+// console.log(dc_heroes)
+// console.log(marvel_heroes)
+
+
+
+
+// let marvelheroes =["thor","ironman","spiderman","antman","Dr.strnage"];
+
+// let val = marvelheroes.shift();
+// console.log("deleted :",val)
+
+
+
+// slice method
+
+// let marvelheroes =["thor","ironman","spiderman","antman","Dr.strnage"];
+
+// console.log(marvelheroes)
+// console.log(marvelheroes.slice(2,))
+// console.log(marvelheroes.slice(4))
+// console.log(marvelheroes.slice(2,4))
+
+
+let arr =[1,2,3,4,5,6];
+
+// arr.splice(2,2,101,102);
+// console.log(arr)
+// Add element
+// arr.splice(2,0,101);
+// console.log(arr)
+
+//delete element
+// arr.splice(3,1)
+// console.log(arr)
+
+
+// replace element
+arr.splice(3,1,104)
+console.log(arr)
---
+++ Day04/Ques02.js
@@ -0,0 +1,23 @@
+
+//  Create an array to store companies -> "Bloomberg","Microsoft","Uber","Google","IBM","netflix"
+// a.Remove the first company from the array
+
+// b. Remove Uber & Add Ola in its place
+
+// c. Add Amazon at the end
+
+ 
+
+let companies = ["Bloomberg","Microsoft","Uber","Google","IBM","netflix" ]
+console.log(companies)
+// companies.shift()
+// console.log(companies)
+
+
+// companies.splice(2,1,"Ola")
+// console.log(companies)
+
+
+companies.push("Amazon");
+console.log(companies)
+
