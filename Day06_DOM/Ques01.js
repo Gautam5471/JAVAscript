c6658b8074211523e2d24fc5a816168f36c37170
Gautam kumar <gautamkumar2020x@gmail.com>
2026-01-17 23:36:39 +0530

    DOM

---
+++ Day06_DOM/Ques01.js
@@ -0,0 +1,16 @@
+//  Create a new button element. Give it a text “click me”, background color of red & text color of white.
+// Insert the button as the first element inside the body tag.
+
+let h2 = document.querySelector("h2");
+
+console.log(h2.innerText);
+h2.innerText = h2.innerText + " from Apna college students ";
+
+
+
+
+
+// Create 3 divs with common class name - “box” . Access them & add some unique text to each of them.
+
+// Create a <p> tag in html, give it a class & some styling.
+// Now create a new class in CSS and try to append this class to the <p> element.
--- Day06_DOM/script.js
+++ Day06_DOM/script.js
@@ -1,3 +1,14 @@
-console.log("hello!")
-alert("hello!")
-console.log(window);
+
+// console.dir(document.body);
+// console.log(document.body); 
+
+// let parag  = document.getElementsByTagName(p)
+// console.log(parag)
+
+
+// let elements = document.querySelectorAll("p");
+// console.log(elements)
+
+
+let fruits = document.querySelector("div")
+console.log(fruits)
